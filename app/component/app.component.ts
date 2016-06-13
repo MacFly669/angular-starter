a1a22f5b0372dabd54a1b4b8574fef00f4b9c59e
MacFly <macfly669@gmail.com>
2016-06-13 14:24:26 +0200

    Files clean&reorganistaion

---
+++ app/component/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  styleUrls: ['app/component/app.component.css'],
+  templateUrl: 'app/component/app.component.html'
+})
+export class AppComponent { }
